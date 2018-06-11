f7794f1c570368575bcfa792182d36ef2b57dee3
FMAGNOUX <magnoux.florent@gmail.com>
2018-06-11 11:31:04 +0200

    fonctions fichier

--- src/Controller/FileController.php
+++ src/Controller/FileController.php
@@ -15,41 +15,97 @@ class FileController extends Controller
     /**
      * @Route("/file/upload", name="app_upload")
      */
+
     public function upload(Request $request)
     {
         $formBuilder = $this->createFormBuilder();
 
+        //these fields must be rendered in the twig (name, fileupload, upload)
         $formBuilder
-            ->add('name',TextType::class)
-            ->add('fileupload',FileType::class)
-            ->add('upload', SubmitType::class, array('label' => 'Ajouter le fichier'));
+            ->add('name', TextType::class)
+            ->add('fileupload', FileType::class)
+            ->add('upload', SubmitType::class, ['label' => 'Ajouter le fichier']);
 
         $form = $formBuilder->getForm();
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+
+            $data = $form->getData();
             $file = new File();
             $user = $this->get('security.token_storage')->getToken()->getUser();
-            $file->setName($form['name']);
 
-            $file = $form['attachment']->getData();
-            $file->move("../public/".$user->getFolder(), $form['name']);
+            $fileUploaded = $data['fileupload'];
+            $file->setName($data['name'].$fileUploaded->guessExtension());
+            if($this->checkUniqueName($file->getName())){
+                $fileUploaded->move('../public/'.$user->getFolder(), $data['name'].$fileUploaded->guessExtension());
 
-            $file->setDateAdd(new \DateTime());
-            $file->setPath("../public/".$user->getFolder()."/".$form['name']);
-            $file->setUser($user);
-            $file->setUserId($user->getId());
+                $file->setDateAdd(new \DateTime());
+                $file->setDateUpdate(new \DateTime());
+                $file->setPath('../public/'.$user->getFolder().'/'.$data['name']);
+                $file->setUser($user);
+                $file->setUserId($user->getId());
+                $file->setSize(filesize('../public/'.$user->getFolder().'/'.$data['name']));
 
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($file);
-            $entityManager->flush();
+                $user->setSpace($user->getSpace() - $file->getSize());
+
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($file, $user);
+                $entityManager->flush();
+
+                return $this->redirectToRoute('app_show');
+            }
+            else {
+                return $this->render('file/upload.html.twig', array(
+                    'form' => $form->createView(),
+                    'errorName' => "Le nom du fichier est déjà existant dans votre espace"
+                ));
+            }
         }
 
-        return $this->render('file/upload.html.twig', array(
-            'form' => $form->createView()
-        ));
+        return $this->render('file/upload.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
 
+    /**
+     * @Route("/file/delete", name="app_file_delete")
+     */
+    public function delete(File $file){
+
+        $user = $this->get('security.token_storage')->getToken()->getUser();
+        $user->setSpace($user->getSpace() + $file->getSize());
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($file);
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        unlink("../public/".$user->getFolder()."/".$file->getName());
+
+        return $this->redirectToRoute('app_show');
+
+    }
+
+    public function update(File $file , Request $request){
+        $formBuilder = $this->createFormBuilder();
+
+        $formBuilder
+            ->add('name', TextType::class, array('data' => $file->getName()))
+            ->add('update', SubmitType::class, ['label' => 'Ajouter le fichier']);
+
+        $form = $formBuilder->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+        }
+
+        return $this->render('file/upload.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 
     /**
@@ -64,5 +120,18 @@ class FileController extends Controller
         return $this->render('file/show.html.twig', array(
             'files' => $files
         ));
+    }
+
+    private function checkUniqueName($nameFile){
+        $files = $this->getDoctrine()
+            ->getRepository(File::class)
+            ->findBy(array('userId' => $this->get('security.token_storage')->getToken()->getUser()->getId()));
+
+        foreach ($files as $key => $value){
+            if($files[$key]->getName() == $nameFile){
+                return false;
+            }
+        }
+        return true;
     }
 }
